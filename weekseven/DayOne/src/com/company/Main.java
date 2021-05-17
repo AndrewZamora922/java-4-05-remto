8aa146105d1937d403efba9794fc0b6bd8319789
Andrew Zamora <anwzamora922@gmail.com>
2021-05-17 10:59:12 -0500

    week seven

---
+++ weekseven/DayOne/src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import com.company.people.Author;
+import com.company.people.Person;
+import com.company.people.Student;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	// Java oops 2
+//        // obj vs class
+//        // An obj is a piece of data -4, "Whom", false [1,2,3,4,]
+//        // A class is what type of data that is - int, String, boolean, Array
+//
+//
+//        // this is the instance of the object
+//        Integer num = new Integer(4);
+//
+//        //return the class
+//        num.getClass();
+
+        // first way of creating a instance of obj
+        Person firstPerson = new Person();
+
+        // the second way
+        Person secondPerson = new Person("John");
+
+        // the last way
+        Person thirdPerson = new Person("John", "Doe", "Phx", 54, 6, false);
+//        thirdPerson.SayHello();
+//        thirdPerson.birthday();
+
+//        System.out.println(thirdPerson.birthday());
+//        System.out.println(thirdPerson.birthYear(2021));
+//        System.out.println(thirdPerson);
+
+
+        Author new_Author = new Author();
+        new_Author.setFirstName("Dan");
+
+
+//        System.out.println(new_Author.writingBook());
+
+
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        Student s1 = new Student();
+        s1.setFirstName("Sara");
+        s1.setAge(19);
+        s1.setGradYear(2025);
+        studentList.add(s1);
+
+        Student s2 = new Student();
+        s2.setFirstName("Ashley");
+        s2.setAge(20);
+        s2.setGradYear(2022);
+        studentList.add(s2);
+
+
+        for (Student s : studentList) {
+            System.out.println("Name: " + s.getFirstName() +
+                    "\nAge: " + s.getAge() + "\ngrad year: " + s.getGradYear());
+        }
+
+    }
+}
---
+++ weekseven/DayOne/src/com/company/people/Person.java
@@ -0,0 +1,121 @@
+package com.company.people;
+
+public class Person {
+    // Default: without an access modifier specified, they are accessible only within the same package
+    //Private: can only be accessed within the class it is declared
+    //Protected: accessible within the same package or sub classes in different packages
+    //Public: can be accessed from every where in the program
+
+    // Constant
+    // Can not be marked static,
+    // because then they'd become class variables
+    // private static final String DATA_TYPE = "Whatever";
+
+    // instance variables
+    private String firstName;
+    private String lastName;
+    private String location;
+    private int age;
+    private double height;
+    private boolean hasJob;
+
+    // The first step of creating an object is the constructor
+    public Person(String firstName, String lastName, String location, int age, double height, boolean hasJob) {
+        // local variables
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.age = age;
+        this.height = height;
+        this.hasJob = hasJob;
+    }
+
+    // One argument constructor
+    public Person(String firstName) {
+
+    }
+
+    // Empty constructor/default constructor
+    public Person() {
+    }
+
+    public void SayHello() {
+        System.out.println("hello " + firstName);
+    }
+
+    public int birthday() {
+        return (age + 1);
+    }
+
+    public int birthday(String month){
+        return (age + 1);
+    }
+
+    public int birthYear(int currentYear) {
+//        int yearOfBirth = currentYear = age;
+//
+//        return yearOfBirth;
+        return currentYear - age;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public boolean isHasJob() {
+        return hasJob;
+    }
+
+    public void setHasJob(boolean hasJob) {
+        this.hasJob = hasJob;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", location='" + location + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", hasJob=" + hasJob +
+                '}';
+    }
+}
