0b45627103b5a4fdc97b34b32dfe6461ea664690
Andrew Zamora <anwzamora922@gmail.com>
2021-05-24 01:17:02 -0500

    Homework Arrays Update

---
+++ Homework/Arrays/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double exampleArray[]= {5,5,5,5,5,5};
+
+        double index = 0;
+
+        for (int i = 0; i<exampleArray.length; i++) {
+            index += exampleArray[i];
+            }
+        System.out.println(index);
+
+
+        }
+
+    }
+
