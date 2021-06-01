e68717bfd3cefa33e66c098d17c6a6e1590dd6f9
Andrew Zamora <anwzamora922@gmail.com>
2021-06-01 00:42:45 -0500

    homework update

--- Homework/Control Flow/src/com/company/AsciiChars.java
+++ Homework/Control Flow/src/com/company/AsciiChars.java
@@ -14,25 +14,10 @@ public class AsciiChars {
     }
 
 
-    String str = "#GeeKs01fOr@gEEks07";
-    int upper = 0, lower = 0, number = 0, special = 0;
-
-    for(int i = 0; i < str.length(); i++) {
-
-        char ch = str.charAt(i);
-        if (ch >= 'A' && ch <= 'Z') {
-            upper++;
-        } else if (ch >= 'a' && ch <= 'z') {
-            lower++;
-        } else if (ch >= '0' && ch <= '9') {
-            number++;
-        } else {
-            special++;
-        }
-    }
+ }
 
 
 
 
 
-}
+
---
+++ weekeight/DayFour/demo/src/main/java/com/example/tts/demospringintro/DemospringintroApplication.java
@@ -0,0 +1,13 @@
+package com.example.tts.demospringintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringintroApplication.class, args);
+	}
+
+}
